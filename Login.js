72a76420cc88b2ac87a51a25697e758f1777fa08
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-09-30 22:05:23 -0300

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,8 @@
+// Agora usando prompt para capturar informações do usuário
+let nomeUsuario = prompt("Digite seu nome:");
+let idadeUsuario = prompt("Digite sua idade:");
+
+// Validação da idade
+if (idadeUsuario >= 18) {
+    alert("Pode tirar a habilitação!");
+}
