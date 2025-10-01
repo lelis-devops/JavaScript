dcb2d09fa5d2493a2d4d2b8f8f1d44258634eab2
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-10-01 18:55:29 -0300

    Delete you.js

--- you.js
+++
@@ -1,8 +0,0 @@
-// Agora usando prompt para capturar informações do usuário
-let nomeUsuario = prompt("Digite seu nome:");
-let idadeUsuario = prompt("Digite sua idade:");
-
-// Validação da idade
-if (idadeUsuario >= 18) {
-    alert("Pode tirar a habilitação!");
-}
