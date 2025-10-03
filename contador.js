607b4a6bd4da64531116ba8f9e141a19fa4e599d
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-10-02 21:42:06 -0300

    Update and rename contador to contador.js

---
+++ contador.js
@@ -0,0 +1,59 @@
+//Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+let contador =1;
+
+while (contador <= 10 ) {
+  alert(contador);
+    contador ++;
+}
+
+
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+let contador =10;
+
+while (contador >= 10 ) {
+  alert(contador);
+    contador --;
+
+}
+
+
+
+
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+  // Pede ao usuário um número para começar a contagem regressiva
+let contador = parseInt(prompt("Digite um número para iniciar a contagem regressiva:"));
+
+
+while (contador >= 0) {
+    console.log(contador); 
+    contador--;            
+
+//Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador
+ let numeroFinal = parseInt(prompt("Digite um número para iniciar a contagem regressiva:")
+ let contador =0;                           
+                while (contador <= numeroFinal) {
+            console.log(contador);
+            contador++; 
+  
+  
+                }
+                         
+                         
+                         
+  //Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+
+let contador = 10;
+let numeroFinal = 0;
+
+while (contador >= numeroFinal) {
+   contagem=contador < numeroFinal
+
+    alert (contador);
+    contador--;            
+
+}
+  
