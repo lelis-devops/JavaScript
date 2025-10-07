705d7e8d75330ad9ec13ac1a8184c84d243c3e79
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-10-06 22:25:14 -0300

    Create jogoupgrade.js

---
+++ jogoupgrade.js
@@ -0,0 +1,14 @@
+//Título HTML + JS
+let titulo = document.querySelector('<h1>');
+titulo.innerHTML 'Jogo do numero secreto';
+
+//Paragrafo HTML + JS
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha entre um número de 1 a 10';
+
+
+// Função (Trecho de código com função)
+function verificarChute() {
+
+
+  }
