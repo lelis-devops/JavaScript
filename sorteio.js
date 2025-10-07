558455aa232d38e77b6ecfa2bc0070dd089a7312
Mileni Lelis Ramos Sérvulo <lelis.engineer@yahoo.com>
2025-10-06 22:42:10 -0300

    Update and rename jogosecreto.js to sorteio.js

---
+++ sorteio.js
@@ -0,0 +1,27 @@
+alert('Seja bem vindo ao sorteio, que a sorte esteja contigo!');
+let numeroMaximo = 70;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo +1);
+console.log(numeroResultante);
+
+let suposição;
+let tentativas = 1;
+
+// enquanto chute não for igual ao número secreto
+while (suposição != numeroSecreto) {
+    chute = parseInt(prompt(`Escolha um número entre 1 e 70 ${numeroMaximo}`));
+
+    if (suposição == numeroSecreto) {
+        break; // se acertar, sai do laço
+    } else {
+        if ( suposição > numeroSecreto) {
+            alert(`O número secreto é menor que ${suposição}`);
+        } else {
+            alert(`O número sorteado é maior que ${suposição}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Meus parabéns! Você descobriu o número sorteado ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
